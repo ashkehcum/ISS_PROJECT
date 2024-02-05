9fc660dca56405a84e42665f8b9bb174dfce1500
ashkechum <156421545+ashkehcum@users.noreply.github.com>
2024-02-05 21:50:34 +0530

    Add files via upload

---
+++ loginsignup.js
@@ -0,0 +1,10 @@
+function switchForm(formId) {
+    document.getElementById('loginForm').style.display = 'none';
+    document.getElementById('signupForm').style.display = 'none';
+    document.getElementById(formId).style.display = 'block';
+}
+let btn=document.querySelector("button");
+btn=>onclick()
+{
+
+}
